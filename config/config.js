70d79788294c7733fbb43c7036b59da81f1d53b7
rakshit kumar <siddhant.kaushik@impronics.co.za>
2025-03-31 15:42:02 +0530

    fix

--- config/config.js
+++ config/config.js
@@ -76,22 +76,22 @@ module.exports = {
 //   // }
 // }
 
-require("dotenv").config(); // Load .env variables
+// require("dotenv").config(); // Load .env variables
 
-module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    // use_env_variable: process.env.DB_DATABASE,
-  },
-};
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//     // use_env_variable: process.env.DB_DATABASE,
+//   },
+// };
