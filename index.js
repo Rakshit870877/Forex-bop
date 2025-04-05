9083c9cbeabc20fb9ef9638fda1f3527876f283c
rakshit kumar <siddhant.kaushik@impronics.co.za>
2025-04-05 13:49:17 +0530

    origin set

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const { exportToExcel } = require("./utils/scheduler");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://forex-impro-test.netlify.app", // replace with your actual Netlify URL
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true, // only if you're using cookies
+  })
+);
+
+// app.use(cors());
 app.get("/", (req, res) => {
   res.send("App is working!");
 });
